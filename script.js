3ba289276c9980e352095d7a6865329f06e776c3
benjaminparker17 <peterparker1612207@gmail.com>
2026-02-09 14:40:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,36 @@
-function checkPassword() {
-  const correctPassword = "raguldivya16252007"; // 🔐 change this
-  const unlockBtn = document.getElementById("unlockBtn");
+document.addEventListener("DOMContentLoaded", function () {
+
+  const correctPassword = "RDforever";
+
+  const checkBtn = document.getElementById("checkBtn");
   const passwordInput = document.getElementById("password");
-  const lockScreen = document.getElementById("lockScreen");
-  const mainContent = document.getElementById("mainContent");
+  const error = document.getElementById("error");
+  const loginBox = document.querySelector(".lock-wrapper");
+  const secretContent = document.getElementById("secret-content");
   const loveBtn = document.getElementById("loveBtn");
+  const popup = document.getElementById("heartPopup");
 
-unlockBtn.addEventListener("click", () => {
-  if (passwordInput.value === correctPassword) {
-    lockScreen.style.display = "none";
-    mainContent.style.display = "block";
-    loveBtn.style.display = "inline-block"; // 💖 show button
-  } else {
-    alert("Wrong password 💔");
-  }
-});
+  // DEBUG CHECK
+  console.log("JS loaded");
 
+  checkBtn.addEventListener("click", function () {
+    console.log("Button clicked");
 
-  if (userPassword === correctPassword) {
-    document.getElementById("login-box").style.display = "none";
-    document.getElementById("secret-content").style.display = "block";
-  } else {
-    error.textContent = "Thappa Podra da Loosu 💔 Try again";
-  }
-}
-function createHeart(side) {
-  const heart = document.createElement("div");
-  heart.classList.add("heart");
-  heart.innerHTML = "💖";
+    if (passwordInput.value === correctPassword) {
+      loginBox.style.display = "none";
+      secretContent.style.display = "block";
+      error.textContent = "";
+    } else {
+      error.textContent = "Wrong password 😝";
+    }
+  });
 
-  heart.style.left = Math.random() * 40 + "px";
-  heart.style.animationDuration = (4 + Math.random() * 4) + "s";
-  heart.style.fontSize = (14 + Math.random() * 16) + "px";
+  loveBtn.addEventListener("click", () => {
+    popup.style.display = "flex";
+  });
 
-  side.appendChild(heart);
+  popup.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
 
-  setTimeout(() => {
-    heart.remove();
-  }, 8000);
-}
-
-const leftSide = document.querySelector(".hearts.left");
-const rightSide = document.querySelector(".hearts.right");
-
-setInterval(() => createHeart(leftSide), 700);
-setInterval(() => createHeart(rightSide), 900);
-const btn = document.getElementById("loveBtn");
-const popup = document.getElementById("heartPopup");
-
-btn.addEventListener("click", () => {
-  popup.style.display = "flex";
-});
-
-popup.addEventListener("click", () => {
-  popup.style.display = "none";
 });
